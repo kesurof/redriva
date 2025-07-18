94cf2a25726bd0a206aa9de1ec2e4d48d1f8f982
kesurof <64525827+kesurof@users.noreply.github.com>
2025-07-19 00:00:27 +0200

    start

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+
+// Sélection dynamique de l'adapter : static si dispo, sinon auto
+let adapter;
+try {
+  adapter = require('@sveltejs/adapter-static');
+} catch (e) {
+  adapter = require('@sveltejs/adapter-auto');
+}
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
   kit: {
     adapter: adapter(),
